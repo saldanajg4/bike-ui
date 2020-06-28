ad0c3577af41d04c6226f3abcc999d5e7c2f9b57
Jose Gabriel <saldanajg4@hotmail.com>
2020-06-28 16:30:27 -0500

    Refactor service.

--- src/app/services/bike.service.ts
+++ src/app/services/bike.service.ts
@@ -17,16 +17,19 @@ export class BikeService {
    * apis
    */
   getBikes(){
-    let token = localStorage.getItem('access_token');
-    return this.http.get('/forward/api/v1/bikes', 
-    {headers: new HttpHeaders().set('Authorization', 'Bearer ' + token)}
-    );
+    // let token = localStorage.getItem('access_token');
+    // return this.http.get('/forward/api/v1/bikes', 
+    // {headers: new HttpHeaders().set('Authorization', 'Bearer ' + token)}
+    // );
+    return this.http.get('/forward/api/v1/bikes', this.setAuthHeaders());
   }
 
   getBike(id: number){
-    let token = localStorage.getItem('access_token');
-    return this.http.get('/forward/api/v1/bikes/' + id,
-    {headers: new HttpHeaders().set('Authorization', 'Bearer ' + token)});
+    // let token = localStorage.getItem('access_token');
+    // return this.http.get('/forward/api/v1/bikes/' + id,
+    // {headers: new HttpHeaders().set('Authorization', 'Bearer ' + token)});
+
+    return this.http.get('/forward/api/v1/bikes/' + id, this.setAuthHeaders());
   }
 
   //this is public
