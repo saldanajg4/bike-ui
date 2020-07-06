8359097caf36b37d7813cada9531a9bfe46576ac
Jose Gabriel Saldana <saldanajg4@hotmail.com>
2020-07-06 18:06:08 -0500

    Docker-compose now implemented.  I had to change the bike service.  Add default config, remove proxy for corss origin.  Now the api-app in controller takes care of that.

--- src/app/services/bike.service.ts
+++ src/app/services/bike.service.ts
@@ -18,24 +18,25 @@ export class BikeService {
    */
   getBikes(){
     // let token = localStorage.getItem('access_token');
-    // return this.http.get('/forward/api/v1/bikes', 
+    return this.http.get('/api/v1/bikes');
     // {headers: new HttpHeaders().set('Authorization', 'Bearer ' + token)}
     // );
-    return this.http.get('/forward/api/v1/bikes', this.setAuthHeaders());
+    // return this.http.get('/forward/api/v1/bikes', this.setAuthHeaders());
   }
 
   getBike(id: number){
     // let token = localStorage.getItem('access_token');
-    // return this.http.get('/forward/api/v1/bikes/' + id,
+    return this.http.get('/api/v1/bikes/' + id);
     // {headers: new HttpHeaders().set('Authorization', 'Bearer ' + token)});
 
-    return this.http.get('/forward/api/v1/bikes/' + id, this.setAuthHeaders());
+    // return this.http.get('/forward/api/v1/bikes/' + id, this.setAuthHeaders());
   }
 
   //this is public
   createBikeRegistration(bike){
     let body = JSON.stringify(bike);
-    return this.http.post('/forward/api/v1/bikes',body, httpOptions);
+    // return this.http.post('/forward/api/v1/bikes',body, httpOptions);
+    return this.http.post('/api/v1/bikes',body, httpOptions);
   }
 
   private getToken(){
